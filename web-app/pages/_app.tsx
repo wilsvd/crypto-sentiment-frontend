d8f5d7d3b7e6c3f404f50ee55f3d0e694f5c4dd1
wilsvd <wilvandijkhuizen@gmail.com>
2023-02-02 14:55:55 +0000

    feat: add a navbar and login design

--- web-app/pages/_app.tsx
+++ web-app/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+import { ThemeProvider } from "next-themes";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from "@nextui-org/react";
+
+import DefaultNavbar from "@/components/DefaultNavbar";
+import SignedInNavbar from "@/components/SignedInNavbar";
+
+function MyApp({ Component, pageProps }: AppProps) {
+	const [login, setLogin] = React.useState(false);
+
+	const Navbar = login ? <SignedInNavbar /> : <DefaultNavbar />;
+	return (
+		// 2. Use at the root of your app
+		<NextUIProvider>
+			{Navbar}
+			<Component {...pageProps} />
+		</NextUIProvider>
+	);
 }
+
+export default MyApp;
---
+++ web-app/pages/signup.tsx
@@ -0,0 +1,5 @@
+import { Input } from "@nextui-org/react";
+
+export default function Signup() {
+	return <Input labelPlaceholder="NextUI" />;
+}
