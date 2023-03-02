4bff2337bca1f08289283f580d35a47addc75091
wilsvd <wilvandijkhuizen@gmail.com>
2023-03-02 20:57:42 +0000

    docs: add docs to discourage bad practice

--- web-app/pages/_app.tsx
+++ web-app/pages/_app.tsx
@@ -9,6 +9,12 @@ import { wrapper } from "@/store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { useStore } from "react-redux";
 
+// Usage with Redux Sage () => Method may be considered unsafe
+// Usage with Redux Persist () => 	"persistence gate is not necessary because the server can already send
+// 									some HTML with some state, so it's better to show it right away and then wait for REHYDRATE action
+// 									to happen to show additional delta coming from persistence storage. That's why we use Server Side Rendering in the first place."
+// 									---- block the UI while rehydration is happening ----
+
 function MyApp({ Component, pageProps }: AppProps) {
 	const store: any = useStore();
 	return (
