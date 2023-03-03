833152d276eac4a3085abbf9052ecb38ee5b0312
wilsvd <wilvandijkhuizen@gmail.com>
2023-03-03 23:42:04 +0000

    refactor: remove unnecessary state persistance after SSR fix

--- web-app/pages/_app.tsx
+++ web-app/pages/_app.tsx
@@ -1,32 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
 
 import Navbar from "@/components/Navbar";
-// 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
-
-import { wrapper } from "@/store/store";
-import { PersistGate } from "redux-persist/integration/react";
-import { useStore } from "react-redux";
-
-// Usage with Redux Sage () => Method may be considered unsafe
-// Usage with Redux Persist () => 	"persistence gate is not necessary because the server can already send
-// 									some HTML with some state, so it's better to show it right away and then wait for REHYDRATE action
-// 									to happen to show additional delta coming from persistence storage. That's why we use Server Side Rendering in the first place."
-// 									---- block the UI while rehydration is happening ----
+import { Provider } from "react-redux";
+import store from "@/store/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	const { store, props } = wrapper.useWrappedStore(pageProps);
 	return (
-		<PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
-			<Provider store={store}>
-				<NextUIProvider>
-					<Navbar />
-					<Component {...props} />
-				</NextUIProvider>
-			</Provider>
-		</PersistGate>
+		<Provider store={store}>
+			<NextUIProvider>
+				<Navbar />
+				<Component {...pageProps} />
+			</NextUIProvider>
+		</Provider>
 	);
 }
 
