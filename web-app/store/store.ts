3fea49c4ed2b570a0dcdac62c30e06f493014caa
wilsvd <wilvandijkhuizen@gmail.com>
2023-03-03 23:47:05 +0000

    refactor: store firebase user instead of individual param

--- web-app/store/store.ts
+++ web-app/store/store.ts
@@ -1,57 +1,23 @@
-import {
-	configureStore,
-	combineReducers,
-	ThunkAction,
-	Action,
-} from "@reduxjs/toolkit";
-import { authSlice } from "./authslice";
-import { createWrapper } from "next-redux-wrapper";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import authReducer from "./authslice";
 
-const rootReducer = combineReducers({
-	[authSlice.name]: authSlice.reducer,
-});
-
-const makeConfiguredStore = () =>
-	configureStore({
-		reducer: rootReducer,
-		devTools: true,
+export function makeStore() {
+	return configureStore({
+		reducer: { auth: authReducer },
 	});
+}
 
-export const makeStore = () => {
-	const isServer = typeof window === "undefined";
+const store = makeStore();
 
-	if (isServer) {
-		return makeConfiguredStore();
-	} else {
-		// we need it only on client side
+export type AppState = ReturnType<typeof store.getState>;
 
-		const persistConfig = {
-			key: "nextjs",
-			whitelist: ["auth"], // make sure it does not clash with server keys
-			storage,
-		};
+export type AppDispatch = typeof store.dispatch;
 
-		const persistedReducer = persistReducer(persistConfig, rootReducer);
-		let store: any = configureStore({
-			reducer: persistedReducer,
-			devTools: process.env.NODE_ENV !== "production",
-		});
-
-		store.__persistor = persistStore(store); // Nasty hack
-
-		return store;
-	}
-};
-
-export type AppStore = ReturnType<typeof makeStore>;
-export type AppState = ReturnType<AppStore["getState"]>;
 export type AppThunk<ReturnType = void> = ThunkAction<
 	ReturnType,
 	AppState,
 	unknown,
-	Action
+	Action<string>
 >;
 
-export const wrapper = createWrapper<AppStore>(makeStore);
+export default store;
