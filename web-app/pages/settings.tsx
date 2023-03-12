dda677d8e5d7ffa0467e82e8fe60e1f362726c83
wilsvd <wilvandijkhuizen@gmail.com>
2023-03-12 20:09:27 +0000

    feat: add delete user account

--- web-app/pages/settings.tsx
+++ web-app/pages/settings.tsx
@@ -1,13 +1,68 @@
 import { selectUser } from "@/store/authslice";
 import { useAppSelector } from "@/store/hooks";
-import { Container, Text } from "@nextui-org/react";
+import { removeUser } from "@/utility/pass_auth";
+import {
+	Container,
+	Text,
+	Input,
+	Modal,
+	Spacer,
+	Button,
+	Checkbox,
+	Row,
+} from "@nextui-org/react";
 import Head from "next/head";
+import { useState } from "react";
 
 export default function AccountSettings() {
-	const user = useAppSelector(selectUser);
+	const user = useAppSelector(selectUser)!;
+
+	const [name, setName] = useState(() => {
+		return user.displayName ? user.displayName : "N/A";
+	});
+	const [email, setEmail] = useState(() => {
+		return user.email ? user.email : "N/A";
+	});
+	const [phone, setPhone] = useState(() => {
+		return user.phoneNumber ? user.phoneNumber : "N/A";
+	});
+	const [readOnly, setReadOnly] = useState(false);
+
+	const [deleteDisabled, setDeleteDisabled] = useState(true);
+
+	function handleChange(event: { target: { name: string; value: string } }) {
+		const { name, value } = event.target;
+
+		switch (name) {
+			case "name":
+				setName(value);
+				break;
+			case "email":
+				setEmail(value);
+				break;
+			case "phone":
+				setPhone(value);
+				break;
+			case "confirmDelete":
+				value == "DELETE"
+					? setDeleteDisabled(false)
+					: setDeleteDisabled(true);
+				break;
+			default:
+				break;
+		}
+	}
+
+	const [visible, setVisible] = useState(false);
+	const handler = () => setVisible(true);
+	const closeHandler = () => {
+		setVisible(false);
+		console.log("closed");
+	};
+
 	return (
-		<Container fluid>
-			<Head>
+		<Container fluid aria-labelledby="setting-container">
+			<Head aria-labelledby="setting-metadata">
 				<title>Account Settings</title>
 				<meta
 					property="og:Settings"
@@ -20,8 +75,76 @@ export default function AccountSettings() {
 			{/* TODO: Improve UI for user account */}
 			{user ? (
 				<>
-					<Text h5>Email: {user?.email}</Text>
-					<Text h5>Name: {user?.displayName}</Text>
+					<Text h5>Display Name</Text>
+					<Input
+						aria-labelledby="setting-name"
+						readOnly={readOnly}
+						name="name"
+						value={name}
+						onChange={handleChange}
+					/>
+					<Spacer y={1}></Spacer>
+					<Text h5>Email Address</Text>
+					<Input
+						aria-labelledby="setting-email"
+						readOnly={readOnly}
+						name="email"
+						value={email}
+						onChange={handleChange}
+					/>
+					<Spacer y={1}></Spacer>
+					<Text h5>Phone Number</Text>
+					<Input
+						aria-labelledby="setting-phone"
+						readOnly={readOnly}
+						name="phone"
+						value={phone}
+						onChange={handleChange}
+					/>
+					<Spacer y={1}></Spacer>
+					<Button auto color="error" shadow onPress={handler}>
+						Delete Account
+					</Button>
+					<Modal
+						closeButton
+						blur
+						aria-labelledby="modal-title"
+						open={visible}
+						onClose={closeHandler}
+					>
+						<Modal.Header>
+							<Text id="modal-title" size={18}>
+								Are you sure you would like to delete your
+								account?
+							</Text>
+						</Modal.Header>
+						<Modal.Body>
+							<Input
+								aria-labelledby="setting-delete"
+								clearable
+								bordered
+								fullWidth
+								color="primary"
+								size="lg"
+								name="confirmDelete"
+								placeholder="Type DELETE to confirm"
+								onChange={handleChange}
+							/>
+						</Modal.Body>
+						<Modal.Footer justify="center">
+							<Button auto flat onPress={closeHandler}>
+								Close
+							</Button>
+							<Button
+								color={"error"}
+								disabled={deleteDisabled}
+								auto
+								onPress={() => removeUser()}
+							>
+								Delete Account
+							</Button>
+						</Modal.Footer>
+					</Modal>
 				</>
 			) : (
 				<>
