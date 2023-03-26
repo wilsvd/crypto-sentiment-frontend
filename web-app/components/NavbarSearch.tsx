17beb3b2605b2f5d359918e22a3fd2dbc4492011
wilsvd <wilvandijkhuizen@gmail.com>
2023-03-26 22:55:45 +0100

    refactor: remove useless imports

--- web-app/components/NavbarSearch.tsx
+++ web-app/components/NavbarSearch.tsx
@@ -18,7 +18,7 @@ import {
 	Table,
 	Text,
 } from "@nextui-org/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function NavbarSearch() {
 	const columns = [
@@ -32,15 +32,7 @@ export default function NavbarSearch() {
 		cryptoDataT[] | null
 	>();
 
-	const user = useAppSelector(selectUser);
-
-	const userFavourites = useAppSelector(selectFavourites);
-	const userFavouritesLoaded = useAppSelector(selectFavLoaded);
-
 	const cryptoData = useAppSelector(selectCryptoData);
-	const cryptoLoaded = useAppSelector(selectCryptoLoaded);
-
-	const dispatch = useAppDispatch();
 
 	function handleChange(event: { target: { name: string; value: string } }) {
 		const value = event.target.value.toLowerCase();
